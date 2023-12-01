812579a298459bd73027be83493cad187fe077f5
whettenr <whettenrmw@gmail.com>
2023-12-01 09:08:30 +0100

    add other run scripts

---
+++ run/run_base_hyperbranchformer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+  
+#SBATCH --job-name=brqb   # nom du job
+#SBATCH --account=nkp@a100
+#SBATCH -C a100
+#SBATCH --gres=gpu:8
+#SBATCH --cpus-per-task=16
+#SBATCH --exclusive
+#SBATCH --time=20:00:00          # temps d'exécution maximum demande (HH:MM:SS) 
+#SBATCH --output=log/brq_%j.log  # log file
+
+module load pytorch-gpu/py3/2.0.1
+conda activate bestrqenv
+
+cd /gpfswork/rech/nkp/uaj64gk/bestrqexp/bestrq
+
+torchrun --nproc_per_node=8 --rdzv_backend c10d --rdzv-endpoint=localhost:0 train.py hparams/best_hyperbranchformer.yaml --find_unused_parameters
---
+++ run/run_w2v2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+  
+#SBATCH --job-name=brqb   # nom du job
+#SBATCH --account=nkp@a100
+#SBATCH -C a100
+#SBATCH --gres=gpu:8
+#SBATCH --cpus-per-task=16
+#SBATCH --exclusive
+#SBATCH --time=20:00:00          # temps d'exécution maximum demande (HH:MM:SS) 
+#SBATCH --output=log/brq_%j.log  # log file
+
+module load pytorch-gpu/py3/2.0.1
+conda activate bestrqenv
+
+cd /gpfswork/rech/nkp/uaj64gk/bestrqexp/bestrq
+
+torchrun --nproc_per_node=8 --rdzv_backend c10d --rdzv-endpoint=localhost:0 wav2vec2/train_sb_wav2vec2.py wav2vec2/hparams/wav2vec2_base.yaml --find_unused_parameters
