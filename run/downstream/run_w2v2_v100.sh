b938680a67ba83e4a231aed9a35c71b17310b3be
whettenr <whettenrmw@gmail.com>
2023-12-01 19:16:27 +0100

    add scripts for downstream

---
+++ run/downstream/run_w2v2_v100.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=brqb   # nom du job
+#SBATCH --account=nkp@v100
+#SBATCH --constraint v100-32g
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=20:00:00          # temps d'exécution maximum demande (HH:MM:SS) 
+#SBATCH --output=log/brqds_%j.log  # log file
+
+module load pytorch-gpu/py3/2.0.1
+conda activate bestrqenv
+
+cd /gpfswork/rech/nkp/uaj64gk/bestrqexp/bestrq
+
+python /gpfswork/rech/nkp/uaj64gk/bestrqexp/benchmark/benchmarks/MP3S/LibriSpeech/LSTM/train_w2v2.py /gpfswork/rech/nkp/uaj64gk/bestrqexp/benchmark/benchmarks/MP3S/LibriSpeech/LSTM/hparams/ssl_w2v2.yaml \
+    --data_folder /gpfsscratch/rech/nkp/uaj64gk/corpus/LibriSpeech/ \
+    --ssl_hub /gpfswork/rech/nkp/uaj64gk/bestrqexp/jz/brqb/CKPT+2023-12-01+16-31-50+00
+
+
