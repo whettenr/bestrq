51e9a7cb82f86cea4bc4bc96f7d4d1d9022a18a0
whettenr <whettenrmw@gmail.com>
2023-12-02 09:05:02 +0100

    edit downstream hypermixing run files

---
+++ run/downstream/run_base_hyperbranch_v100.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=brqbf   # nom du job
+#SBATCH --account=nkp@v100
+#SBATCH --constraint v100-32g
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=20:00:00          # temps d'exécution maximum demande (HH:MM:SS) 
+#SBATCH --output=log/brqds_%j.log  # log file
+
+module load pytorch-gpu/py3/2.0.1
+conda activate bestrqenv
+
+cd /gpfswork/rech/nkp/uaj64gk/bestrqexp/bestrq
+
+python /gpfswork/rech/nkp/uaj64gk/bestrqexp/benchmark/benchmarks/MP3S/LibriSpeech/LSTM/train_brq.py /gpfswork/rech/nkp/uaj64gk/bestrqexp/benchmark/benchmarks/MP3S/LibriSpeech/LSTM/hparams/ssl_brq_hyperbranchformer.yaml \
+    --data_folder /gpfsscratch/rech/nkp/uaj64gk/corpus/LibriSpeech/ \
+    --ssl_hub /gpfswork/rech/nkp/uaj64gk/bestrqexp/jz/brqhb/CKPT+2023-12-01+16-26-37+00
+
