a6b01af558aa13fed351685b24cf4decfb40a803
whettenr <whettenrmw@gmail.com>
2023-12-08 14:37:31 +0100

    update seed on run script

--- run/finetune/run_wide_v100.sh
+++ run/finetune/run_wide_v100.sh
@@ -16,7 +16,7 @@ cd /gpfswork/rech/nkp/uaj64gk/bestrqexp/bestrq
 python /gpfswork/rech/nkp/uaj64gk/bestrqexp/bestrq/finetune/brq/finetune_bestrq.py /gpfswork/rech/nkp/uaj64gk/bestrqexp/bestrq/finetune/brq/finetune_bestrq.yaml \
     --data_folder /gpfsscratch/rech/nkp/uaj64gk/corpus/LibriSpeech/ \
     --pt_model_hub /gpfswork/rech/nkp/uaj64gk/bestrqexp/jz/brqwide \
-    --d_model 768 --pt_model_output_dim 768 --seed 5000 \
+    --d_model 768 --pt_model_output_dim 768 --seed 5010 \
     --use_language_modelling False --ngram_lm_path /gpfswork/rech/nkp/uaj64gk/bestrqexp/4-gram.arpa.gz
 
     # --test_only --use_language_modelling True --ngram_lm_path /gpfswork/rech/nkp/uaj64gk/bestrqexp/4-gram.arpa.gz
