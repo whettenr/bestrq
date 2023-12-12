c74506672e2b5b1beafa1c7dc23c0f4d158db923
whettenr <whettenrmw@gmail.com>
2023-12-12 13:12:21 +0100

    add brq multinode scripts

---
+++ run/multinode/run_brq.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+module load pytorch-gpu/py3/2.0.1
+conda activate bestrqenv
+
+cd /gpfswork/rech/nkp/uaj64gk/bestrqexp/bestrq
+
+# We extract the master node address (the one that every node must connects to)
+LISTNODES=`scontrol show hostname $SLURM_JOB_NODELIST`
+MASTER=`echo $LISTNODES | cut -d" " -f1`
+
+# here --nproc_per_node=4 because we want torchrun to spawn 4 processes (4 GPUs). Then we give the total amount of nodes requested (--nnodes) and then --node_rank that is necessary to dissociate the node that we are calling this from.
+python -m torch.distributed.run --nproc_per_node=4 --nnodes=${SLURM_JOB_NUM_NODES} --node_rank=${SLURM_NODEID} --master_addr=${MASTER} --master_port=5555 train.py hparams/best_rq_base.yaml \
+    --find_unused_parameters --bfloat16_mix_prec --output_folder results/brq-a100 --grad_accumulation_factor=1
---
+++ run/multinode/sbatch_brq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=bmn   # nom du job
+#SBATCH --nodes=3
+#SBATCH --account=nkp@a100
+#SBATCH -C a100
+# SBATCH --partition=gpu_p4
+#SBATCH --gres=gpu:8
+#SBATCH --cpus-per-task=10
+#SBATCH --exclusive
+#SBATCH --time=20:00:00          # temps d'exécution maximum demande (HH:MM:SS) 
+#SBATCH --output=log/brq_%j.log  # log file
+
+
+cd /gpfswork/rech/nkp/uaj64gk/bestrqexp/bestrq/run/multinode
+srun run_w2v2.sh
